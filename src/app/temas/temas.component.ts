272497c27185e7e8409a476cfc17ee00dcf086f5
Victor Martins <vitor_jhoul@hotmail.com>
2022-05-05 15:07:28 -0300

    finalizando montagem do componente tema

--- src/app/temas/temas.component.ts
+++ src/app/temas/temas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-temas',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemasComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+
+    // if(environment.token ==''){
+    //   alert('Sessão expirada, faça login para continuar')
+    //   this.router.navigate(['/entrar'])
+    // }
   }
-
 }
+
+
